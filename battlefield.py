7b8f83449cebe75b3f98789b10bdb474c30d94fa
Derek Bohman <derekbohman@Dereks-MacBook-Pro.local>
2022-03-22 15:27:12 -0700

    Updated notes for one-on-one with instructor. Reached a point where I'm unsure of what to do.

--- battlefield.py
+++ battlefield.py
@@ -3,15 +3,15 @@ from herd import Herd
 
 class Battlefield:
     def __init__(self):
-        self.fleet = Fleet() #?
-        self.herd = Herd() #?
+        self.fleet = Fleet() #Not sure if this is correct.
+        self.herd = Herd() #Not sure if this is correct.
     def run_game(self):
         self.display_welcome()
     def display_welcome(self):
         print("")
         print("Welcome to Dinos vs. Robos")
         print("")
-    def battle(self):
+    def battle(self): #Does this represent the actual fighting or does it represent choosing options like who to attack?
         pass
     def dino_turn(self, dinosaur):
         pass
